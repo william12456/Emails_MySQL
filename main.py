e14f136bfb53919a8f80d4d23314e3419df59aa8
Pablo <dev.pablobelmiro@gmail.com>
2024-01-15 20:48:24 -0300

    atualização

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pymysql
+from pathlib import Path
+import json
+from datetime import date  # Adicionando esta linha para importar o tipo de dado date
 
+# Definindo path
+path = Path.cwd() / 'arquivo.json'
 # Conectar ao banco de dados
 connection = pymysql.connect(
     host="localhost",
@@ -10,7 +15,7 @@ connection = pymysql.connect(
 
 # Executar consultas
 with connection.cursor() as cursor:
-    sql = "SELECT nome, cnpj, cidade FROM granlog.pessoa LIMIT 1"
+    sql = "SELECT tipo_contato.Descricao, pessoa_contato.Valor, Nome, DataNascimento, CPF, Identidade, Emissor, DataEmissao, Cep, Endereco, Numero, Complemento, Bairro, Municipio, UF FROM testesql.pessoa inner join testesql.pessoa_contato ON pessoa.Id_Pessoa = pessoa_contato.Id_Pessoa inner join testesql.tipo_contato ON pessoa_contato.Id_Tipo_Contato = tipo_contato.Id_Tipo_Contato WHERE tipo_contato.Id_Tipo_Contato = 3"    
     cursor.execute(sql)
     result = cursor.fetchall()
 
@@ -20,12 +25,19 @@ columns = [column[0] for column in cursor.description]
 # Criar lista de dicionários
 result_dict_list = []
 for row in result:
-    row_dict = dict(zip(columns, row))
+    row_dict = {}
+    for i in range(len(columns)):
+        # Verificar se o valor é do tipo date e convertê-lo para string
+        if isinstance(row[i], date):
+            row_dict[columns[i]] = row[i].isoformat()
+        else:
+            row_dict[columns[i]] = row[i]
+
     result_dict_list.append(row_dict)
 
 # Fechar a conexão
 connection.close()
 
-# Imprimir o resultado como objetos chave-valor
-for item in result_dict_list:
-    print(item[''])
+with open(path, 'w') as arquivo:
+    # Imprimir o resultado como objetos chave-valor
+    json.dump(result_dict_list, arquivo, indent=2)
